c98bd20956d4491839a9a37747daefb7f06edfde
Jonathan Knowles <mail@jonathanknowles.net>
2024-12-04 01:09:33 +0000

    Waypoint.

--- lib/internal/Data/Sign/Internal.hs
+++ lib/internal/Data/Sign/Internal.hs
@@ -8,3 +8,19 @@ data Sign
     | Zero
     | Positive
     deriving stock (Bounded, Enum, Eq, Ord, Show)
+
+{- ORMOLU_DISABLE -}
+multiply :: Sign -> Sign -> Sign
+multiply = \case
+    Negative -> invert
+    Zero     -> const Zero
+    Positive -> id
+{- ORMOLU_ENABLE -}
+
+{- ORMOLU_DISABLE -}
+invert :: Sign -> Sign
+invert = \case
+    Negative -> Positive
+    Zero     -> Zero
+    Positive -> Negative
+{- ORMOLU_ENABLE -}
