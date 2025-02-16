47ed499877dda80ad259d43b6d1cbe220cff976b
Jonathan Knowles <mail@jonathanknowles.net>
2025-02-16 00:57:42 +0000

    More operations for SeqMap.

--- lib/public/Data/Map/SeqMap.hs
+++ lib/public/Data/Map/SeqMap.hs
@@ -1,11 +1,49 @@
-module Data.Map.SeqMap where
+module Data.Map.SeqMap
+    ( SeqMap
+    , fromList
+    , toList
+    , keySet
+    , keyBag
+    , valueSet
+    , valueBag
+    , isPrefixOf
+    , isSuffixOf
+    , isProperPrefixOf
+    , isProperSuffixOf
+    , stripPrefix
+    , stripSuffix
+    , commonPrefix
+    , commonSuffix
+    , stripCommonPrefix
+    , stripCommonSuffix
+    , overlap
+    , stripPrefixOverlap
+    , stripSuffixOverlap
+    , stripOverlap
+    )
+where
 
+import Data.Map.Strict qualified as Map
 import Data.Monoid.GCD
     ( LeftDistributiveGCDMonoid
     , LeftGCDMonoid
     , OverlappingGCDMonoid
     , RightDistributiveGCDMonoid
     , RightGCDMonoid
+    )
+import Data.Monoid.GCD qualified as LeftGCDMonoid
+    ( commonPrefix
+    , stripCommonPrefix
+    )
+import Data.Monoid.GCD qualified as OverlappingGCDMonoid
+    ( overlap
+    , stripOverlap
+    , stripPrefixOverlap
+    , stripSuffixOverlap
+    )
+import Data.Monoid.GCD qualified as RightGCDMonoid
+    ( commonSuffix
+    , stripCommonSuffix
     )
 import Data.Monoid.Null
     ( MonoidNull
@@ -21,11 +59,24 @@ import Data.Semigroup.Cancellative
     , RightCancellative
     , RightReductive
     )
-import Data.Semigroup.Cancellative qualified as C
+import Data.Semigroup.Cancellative qualified as LeftReductive
+    ( isPrefixOf
+    , stripPrefix
+    )
+import Data.Semigroup.Cancellative qualified as RightReductive
+    ( isSuffixOf
+    , stripSuffix
+    )
 import Data.Sequence
     ( Seq
     )
-import Data.Set (Set)
+import Data.Sequence qualified as Seq
+import Data.Set
+    ( Set
+    )
+import Internal.Data.Count
+    ( Count (Count)
+    )
 import Internal.Data.Packed
     ( Packed (Unpacked)
     )
@@ -33,8 +84,6 @@ import Internal.Shared
     ( Bag (Bag)
     )
 import Prelude
-import Internal.Data.Count (Count(Count))
-import qualified Data.Sequence as Seq
 
 newtype SeqMap k v = SeqMap (MonoidMap k (Seq v))
     deriving newtype
@@ -86,13 +135,63 @@ valueBag :: SeqMap k v -> Bag v
 valueBag = undefined
 
 isPrefixOf :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> Bool
-isPrefixOf = C.isPrefixOf
-
-isProperPrefixOf :: SeqMap k v -> SeqMap k v -> Bool
-isProperPrefixOf = undefined
+isPrefixOf = LeftReductive.isPrefixOf
 
 isSuffixOf :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> Bool
-isSuffixOf = C.isSuffixOf
+isSuffixOf = RightReductive.isSuffixOf
 
-isProperSuffixOf :: SeqMap k v -> SeqMap k v -> Bool
-isProperSuffixOf = undefined
+isProperPrefixOf :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> Bool
+isProperPrefixOf (SeqMap m1) (SeqMap m2) =
+    Map.isProperSubmapOfBy
+        LeftReductive.isPrefixOf
+        (MonoidMap.toMap m1)
+        (MonoidMap.toMap m2)
+
+isProperSuffixOf :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> Bool
+isProperSuffixOf (SeqMap m1) (SeqMap m2) =
+    Map.isProperSubmapOfBy
+        RightReductive.isSuffixOf
+        (MonoidMap.toMap m1)
+        (MonoidMap.toMap m2)
+
+stripPrefix :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> Maybe (SeqMap k v)
+stripPrefix = LeftReductive.stripPrefix
+
+stripSuffix :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> Maybe (SeqMap k v)
+stripSuffix = RightReductive.stripSuffix
+
+commonPrefix :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> SeqMap k v
+commonPrefix = LeftGCDMonoid.commonPrefix
+
+commonSuffix :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> SeqMap k v
+commonSuffix = RightGCDMonoid.commonSuffix
+
+stripCommonPrefix
+    :: (Ord k, Eq v)
+    => SeqMap k v
+    -> SeqMap k v
+    -> (SeqMap k v, SeqMap k v, SeqMap k v)
+stripCommonPrefix = LeftGCDMonoid.stripCommonPrefix
+
+stripCommonSuffix
+    :: (Ord k, Eq v)
+    => SeqMap k v
+    -> SeqMap k v
+    -> (SeqMap k v, SeqMap k v, SeqMap k v)
+stripCommonSuffix = RightGCDMonoid.stripCommonSuffix
+
+overlap :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> SeqMap k v
+overlap = OverlappingGCDMonoid.overlap
+
+stripPrefixOverlap :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> SeqMap k v
+stripPrefixOverlap = OverlappingGCDMonoid.stripPrefixOverlap
+
+stripSuffixOverlap :: (Ord k, Eq v) => SeqMap k v -> SeqMap k v -> SeqMap k v
+stripSuffixOverlap = OverlappingGCDMonoid.stripSuffixOverlap
+
+stripOverlap
+    :: (Ord k, Eq v)
+    => SeqMap k v
+    -> SeqMap k v
+    -> (SeqMap k v, SeqMap k v, SeqMap k v)
+stripOverlap = OverlappingGCDMonoid.stripOverlap
